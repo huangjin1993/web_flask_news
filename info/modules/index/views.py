108d0a5f25a2ab24156cda802a8abf3a45ae6a8b
黄进 <930179651@qq.com>
2019-06-07 01:06:12 +0800

    1.渲染首页.创建模板文件夹,把static中的news首页拖到模板文件夹中

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,8 +1,10 @@
+from flask import render_template
+
 from info import redis_store
 from info.modules.index import index_blu
 
 
 @index_blu.route("/")
 def index():
-    redis_store.set("name","laozhang")
-    return "index"
+
+    return render_template("news/index.html")
