517ab13d75c2bfeebf8119b0b1f67acaf3b435c3
黄进 <930179651@qq.com>
2019-06-06 15:16:21 +0800

    1.使用工厂方法用来创建不同配置的app

--- info/__init__.py
+++ info/__init__.py
@@ -6,14 +6,19 @@ from flask_session import Session
 from config import config
 
 
-app = Flask(__name__)
-# 1.集成配置类
-app.config.from_object(config["develop"])
-# 2.集成sqlalchemy
-db = SQLAlchemy(app)
-# 3.集成redis
-redis_store = StrictRedis(host=config["develop"].REDIS_HOST,port=config["develop"].REDIS_PORT)
-# 4.SCRFProtect,用来以后设置校验值
-CSRFProtect(app)
-# 5.集成flask_session
-Session(app)
+db = SQLAlchemy()
+
+def create_app(config_name):
+    app = Flask(__name__)
+    # 1.集成配置类
+    app.config.from_object(config[config_name])
+    # 2.集成sqlalchemy
+    db.init_app(app)
+    # 3.集成redis
+    redis_store = StrictRedis(host=config[config_name].REDIS_HOST,port=config[config_name].REDIS_PORT)
+    # 4.SCRFProtect,用来以后设置校验值
+    CSRFProtect(app)
+    # 5.集成flask_session
+    Session(app)
+
+    return app
--- manage.py
+++ manage.py
@@ -11,9 +11,9 @@
 
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from info import app, db
+from info import create_app, db
 
-
+app = create_app("develop")
 # 6.集成flask_script
 manager = Manager(app)
 # 7.集成数据库迁移flask_migrate,在flask中对数据库迁移
