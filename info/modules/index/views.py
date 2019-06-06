c670a124d94bcde64f7d48b89ebbc99b70ca3ffb
黄进 <930179651@qq.com>
2019-06-07 02:17:03 +0800

    1.favicon.ico小图标显示,用的重定向(一般不用这个方法),current_app.send_static_file(图标路径),其余方法待验证

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, send_file, redirect
+from flask import render_template, send_file, redirect, current_app
 
 from info import redis_store
 from info.modules.index import index_blu
@@ -15,6 +15,6 @@ def favicon():
     # 1.重定向(一般不用)
     # return redirect("/static/news/favicon.ico")
     # 2.
-    # return current_app.send_static_file("news/favicon.ico")
-    # 3.
-    return send_file("news/favicon.ico")
+    return current_app.send_static_file("news/favicon.ico")
+    # 3.实现不了,方法估计有问题
+    # return send_file("news/favicon.ico")
