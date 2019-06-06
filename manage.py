0f06c30492745f25b9d87a7786818913290f8b1e
黄进 <930179651@qq.com>
2019-06-06 09:27:38 +0800

    1.集成配置类 2.集成sqlalchemy

--- manage.py
+++ manage.py
@@ -1,7 +1,26 @@
+"""
+1.集成配置类
+2.集成sqlalchemy
+
+"""
+
+
 from flask import Flask
+from  flask_sqlalchemy import SQLAlchemy
+
+
+class Config(object):
+    DEBUG = True
+
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/flask_news'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
 app = Flask(__name__)
+# 1.集成配置类
+app.config.from_object(app)
+# 2.集成sqlalchemy
+db = SQLAlchemy(app)
 
 
 @app.route("/")
@@ -10,4 +29,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
