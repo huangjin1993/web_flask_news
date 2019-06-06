56279f055fd3463d32d7f84f123a80271b02bea4
黄进 <930179651@qq.com>
2019-06-07 02:05:33 +0800

    1.favicon.ico小图标显示,用的重定向,用其余两种方法访问不到待解决

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, send_file, redirect
 
 from info import redis_store
 from info.modules.index import index_blu
@@ -8,3 +8,13 @@ from info.modules.index import index_blu
 def index():
 
     return render_template("news/index.html")
+
+
+@index_blu.route("/favicon.ico")
+def favicon():
+    # 1.重定向(一般不用)
+    # return redirect("/static/news/favicon.ico")
+    # 2.
+    # return current_app.send_static_file("news/favicon.ico")
+    # 3.
+    return send_file("news/favicon.ico")
