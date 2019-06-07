613ee646e134996210e92847611e799c8824b5fd
黄进 <930179651@qq.com>
2019-06-07 17:04:21 +0800

    1.验证码添加到response-header中返回给浏览器,修改Content-Type:属性

--- info/modules/passport/views.py
+++ info/modules/passport/views.py
@@ -1,4 +1,4 @@
-from flask import request, abort, current_app
+from flask import request, abort, current_app, make_response
 from info import redis_store, constants
 from info.modules.passport import passport_blu
 from info.utils.captcha.captcha import captcha
@@ -20,6 +20,8 @@ def get_image_code():
         current_app.logger.error(e)
         abort(500)
     # 5.把验证图片返回给浏览器
-    return image
+    response = make_response(image)
+    response.headers["Content-Type"] = "image/jpg"
+    return response
 
 
