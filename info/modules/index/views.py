7a040660b16ff7bb5be03ff956e25b06249e23bd
黄进 <930179651@qq.com>
2019-06-10 10:29:10 +0800

    1.右上角用户登录状态的实现

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,13 +1,29 @@
-from flask import render_template, send_file, redirect, current_app
+from flask import render_template, send_file, redirect, current_app, session
 
 from info import redis_store
+from info.models import User
 from info.modules.index import index_blu
 
 
 @index_blu.route("/")
 def index():
+    """
+    需求:首页右上角实现
+    当进入到首页,判断是否登录,如果登录,将用户的信息查出来渲染index.html
+    :return:
+    """
+    user_id = session.get("user_id")
+    user = None
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+    data = {
+        "user_info": user.to_dict() if user else None
+    }
 
-    return render_template("news/index.html")
+    return render_template("news/index.html",data = data)
 
 
 @index_blu.route("/favicon.ico")
@@ -16,5 +32,5 @@ def favicon():
     # return redirect("/static/news/favicon.ico")
     # 2.
     return current_app.send_static_file("news/favicon.ico")
-    # 3.实现不了,方法估计有问题
+    # 3.
     # return send_file("/static/news/favicon.ico")
